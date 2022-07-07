452466bb007aacd73f491d61eff77b47b71efcea
Valetina-2608 <valusha2608@gmail.com>
2022-07-07 23:28:50 +0300

    Add function openBasket

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 /* Script */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Show and hide modal_basket */
+
+let basket = document.querySelector('.basket');
+basket.addEventListener('click', openBasket);
+	
+function openBasket(){
+     if(basket_modal.style.display ==='none'){
+            basket_modal.style.display = "block";
+			basket_modal.classList.add('modal_show');
+      }else{
+            basket_modal.style.display = "none";
+      }
+}
+
+
+
+
+
